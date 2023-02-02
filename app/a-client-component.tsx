575696577997150dbcf565d3be34814b85a06024
Patrick Arminio <patrick.arminio@gmail.com>
2023-02-02 12:21:45 +0000

    Initial commit

---
+++ app/a-client-component.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+
+export const AClientComponent = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
